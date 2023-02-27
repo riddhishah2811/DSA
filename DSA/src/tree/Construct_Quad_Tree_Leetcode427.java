1a0fdc1bb80957b05541c7d90625918001b8ef80
riddhishah2811 <riddhishah2222@gmail.com>
2023-02-28 01:56:55 +0530

    Optimized  Construct_Quad_Tree_Leetcode427 added

--- DSA/src/tree/Construct_Quad_Tree_Leetcode427.java
+++ DSA/src/tree/Construct_Quad_Tree_Leetcode427.java
@@ -53,7 +53,13 @@ public class Construct_Quad_Tree_Leetcode427 {
     }
     public static Node util(int[][] grid,int x1,int y1,int len)
     {
-        if(sameValue(grid,x1,y1,len))
+    	// if(sameValue(grid,x1,y1,len))
+        // {
+        //     return new Node(grid[x1][y1] == 1, true, null, null, null, null);
+        // }
+
+        // Optimized
+        if(len==1)
         {
             return new Node(grid[x1][y1] == 1, true, null, null, null, null);
         }
@@ -64,7 +70,13 @@ public class Construct_Quad_Tree_Leetcode427 {
             n.topRight = util(grid,x1,y1+(len/2),len/2);
             n.bottomLeft = util(grid,x1 + (len/2),y1 ,len/2);
             n.bottomRight = util(grid,x1 + (len/2),y1 + (len/2), len/2);
-             return n;
+            
+            //Optimized
+            if(n.topLeft.isLeaf==true && n.topRight.isLeaf==true && n.bottomLeft.isLeaf==true && n.bottomRight.isLeaf==true &&
+            n.topLeft.val==n.topRight.val && n.topRight.val==n.bottomLeft.val && n.bottomLeft.val==n.bottomRight.val){
+                return new Node(n.topLeft.val, true, null, null, null, null);
+            }
+             return new Node(n.topLeft.val,false,n.topLeft,n.topRight,n.bottomLeft,n.bottomRight);
 
         }
     }
